d1335944a2625805762c663f9f3c9f98533423c8
Daniel Mason <daniel@danielmason.com>
2019-08-11 15:34:12 +0100

    feat(cli): add stuctopt to provide easy command line interface

--- src/bin/crawl.rs
+++ src/bin/crawl.rs
@@ -1,5 +1,13 @@
 use web_crawler::crawl;
+use structopt::StructOpt;
+
+#[derive(Debug, StructOpt)]
+struct Opts {
+    /// The url to crawl
+    url: String,
+}
 
 fn main() {
-    crawl("");
+    let opts = Opts::from_args();
+    crawl(&opts.url);
 }
